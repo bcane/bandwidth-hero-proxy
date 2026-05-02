13134d7c48605870e7bbc8b7a62312672bf0266d
bcane <57240625+bcane@users.noreply.github.com>
2026-05-02 12:12:15 +1000

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -11,7 +11,7 @@ exports.handler = async (event, context) => {
 
     if (!url) {
         return {
-            statusCode: 204,
+            statusCode: 200,
             body: "bandwidth-hero-proxy"
         };
     }
